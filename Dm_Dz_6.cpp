56a37a8ea167fd0b2007536ecf2974621417e1c6
FerKosNik <ferafontov83@mail.ru>
2021-11-11 23:48:25 +0300

    Create Dm_Dz_6

---
+++ Dm_Dz_6.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <mutex>
+
+using namespace std;
+
+once_flag callflag;
+
+void once_print()
+{
+    cout << '!';
+}
+void print(size_t x)
+{
+    call_once(callflag, once_print);
+    cout << x;
+}
+
+int main()
+{
+    vector<thread> v;
+    for (size_t i = 0; i < 10; ++i) {
+        v.push_back(thread(print, i));
+    }
+    for (auto& t : v) { t.join(); }
+    cout << endl;
+}
